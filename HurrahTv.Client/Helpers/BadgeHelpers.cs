baef2267b3479a3750e2357dfd9919ef91cf3a8a
Mike Kerchenski <mkerchenski@hey.com>
2026-04-07 16:42:45 -0400

    Refactor status badges and update UI colors
    Introduce BadgeHelpers for status badge logic, replacing inline code in ShowPicker and WatchlistRow. Update "new episode" icon color to amber-300 for better visibility. Reduce badge and icon sizes in PosterCard. Improve WatchlistRow badge display and sentiment icon placement. Shorten upcoming episode date labels. Add new color variables and responsive utility classes in app.css. Update manifest short_name to "Hurrah.tv".

---
+++ HurrahTv.Client/Helpers/BadgeHelpers.cs
@@ -0,0 +1,42 @@
+using HurrahTv.Shared.Models;
+
+namespace HurrahTv.Client.Helpers;
+
+public static class BadgeHelpers
+{
+    public static string StatusBg(QueueStatus status) => status switch
+    {
+        QueueStatus.WantToWatch => "bg-accent/80",
+        QueueStatus.Watching    => "bg-green-600/80",
+        QueueStatus.Finished    => "bg-blue-600/80",
+        QueueStatus.NotForMe    => "bg-gray-600/80",
+        _                       => "bg-gray-600/80"
+    };
+
+    public static string StatusIcon(QueueStatus status) => status switch
+    {
+        QueueStatus.WantToWatch => "icon-[heroicons--bookmark-20-solid]",
+        QueueStatus.Watching    => "icon-[heroicons--play-20-solid]",
+        QueueStatus.Finished    => "icon-[heroicons--check-20-solid]",
+        QueueStatus.NotForMe    => "icon-[heroicons--x-mark-20-solid]",
+        _                       => ""
+    };
+
+    public static string StatusShortLabel(QueueStatus status) => status switch
+    {
+        QueueStatus.WantToWatch => "Want",
+        QueueStatus.Watching    => "Watching",
+        QueueStatus.Finished    => "Watched",
+        QueueStatus.NotForMe    => "Nope",
+        _                       => ""
+    };
+
+    public static string StatusRing(QueueStatus status) => status switch
+    {
+        QueueStatus.WantToWatch => "ring-accent",
+        QueueStatus.Watching    => "ring-green-500",
+        QueueStatus.Finished    => "ring-blue-500",
+        QueueStatus.NotForMe    => "ring-gray-500",
+        _                       => "ring-gray-500"
+    };
+}
